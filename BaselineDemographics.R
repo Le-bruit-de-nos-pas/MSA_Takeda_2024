c6b53b58292296c6a890d3b899ba10f9ece35991
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2024-02-24 20:12:08 +0100

    Add files via upload

---
+++ BaselineDemographics.R
@@ -0,0 +1,407 @@
+
+library(tidyverse)
+library(data.table)
+options(scipen = 999)
+
+# Inputs, cleanup ----------------------------------
+
+AllMSA_Pop_Baseline_671 <- fread("Source/AllMSA_Pop_Baseline_671.txt")
+EarlyCT_Pop_Baseline_319 <- fread("Source/EarlyCT_Pop_Baseline_319.txt")
+dataCohorteManaged <- readRDS("Source/dataCohorteManaged.rds")
+
+
+BaselineDem <- dataCohorteManaged  %>% 
+  select(NUM, DATECONSULT, DATENAIS, SEXE, DATE_VISITE0, annee_vis0, ANDEBSYMPT, AGE_VISITE0, DELAI_CHARGE) %>%
+  mutate(AGE_DEBSYMPT=AGE_VISITE0-DELAI_CHARGE)  %>%
+  inner_join(AllMSA_Pop_Baseline_671) 
+
+# First visit, to replace missing visit 0s
+BaselineDem <- BaselineDem %>% left_join(BaselineDem %>% group_by(NUM) %>% summarise(MIN_DATECONSULT=min(DATECONSULT))) 
+
+# Distinct
+BaselineDem <- BaselineDem %>% select(-DATECONSULT) %>% distinct()
+
+# Year of symptom onset, min because some have different ones
+BaselineDem <- BaselineDem %>% group_by(NUM) %>% mutate(ANDEBSYMPT=min(ANDEBSYMPT, na.rm=T))
+  
+# Year of first visit, min because some have different ones
+BaselineDem <- BaselineDem %>% group_by(NUM) %>% mutate(annee_vis0=min(annee_vis0, na.rm=T))
+
+# Years elapsed from symptom to visit 
+BaselineDem <- BaselineDem %>% group_by(NUM) %>% mutate(DELAI_CHARGE=annee_vis0-ANDEBSYMPT)
+
+# missing dates of first visit
+BaselineDem <- BaselineDem %>% group_by(NUM) %>% mutate(DATE_VISITE0=if_else(is.na(DATE_VISITE0), MIN_DATECONSULT, DATE_VISITE0))
+
+# age at first visit; calculate for missing ones
+BaselineDem <- BaselineDem %>%  group_by(NUM) %>%
+  mutate(AGE_VISITE0=if_else(is.na(AGE_VISITE0), as.numeric(DATE_VISITE0-DATENAIS)/365.25, AGE_VISITE0))
+
+BaselineDem <- BaselineDem %>% group_by(NUM) %>%
+  mutate(AGE_DEBSYMPT=if_else(is.na(AGE_DEBSYMPT), as.numeric(AGE_VISITE0-DELAI_CHARGE), AGE_DEBSYMPT))
+
+BaselineDem <- BaselineDem %>% group_by(NUM) %>% mutate(AGE_DEBSYMPT=min(AGE_DEBSYMPT, na.rm=T))
+
+names(BaselineDem)
+range(BaselineDem$DATENAIS)
+range(BaselineDem$DATE_VISITE0)
+range(BaselineDem$annee_vis0)
+range(BaselineDem$ANDEBSYMPT)
+range(BaselineDem$DELAI_CHARGE)
+range(BaselineDem$MIN_DATECONSULT)
+range(BaselineDem$AGE_VISITE0)
+range(BaselineDem$AGE_DEBSYMPT)
+
+data.frame(
+  AllMSA_Pop_Baseline_671 %>% 
+  inner_join(BaselineDem) %>%
+    distinct()  %>% 
+  group_by(NUM) %>% count() %>% filter(n>1) %>%
+  left_join(BaselineDem) 
+  )
+
+# --------------
+
+# SUMMARY STATISTICS ---------------
+
+# Overall -------------------
+
+# EarlyCT 319
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(SEXE) %>% count()
+
+
+# ALL MSA 671
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  group_by(SEXE) %>% 
+  count()
+
+# ----------------------
+
+# By DIAG TYPE ------------
+
+
+# EarlyCT 319
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAG, SEXE) %>% count()
+
+
+# ALL MSA 671
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAG) %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  group_by(DIAG, SEXE) %>% 
+  count()
+
+
+# --------------
+
+
+# BY DIAG NIV -------------
+
+
+
+# EarlyCT 319
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  inner_join(EarlyCT_Pop_Baseline_319) %>%
+  group_by(DIAGNIV, SEXE) %>% count()
+
+
+# ALL MSA 671
+
+
+# AGE_VISITE0
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(AGE_VISITE0),
+    sd=sd(AGE_VISITE0),
+    median=median(AGE_VISITE0),
+    Q1 = quantile(AGE_VISITE0, 0.25),
+    Q3 = quantile(AGE_VISITE0, 0.75),
+    min=min(AGE_VISITE0),
+    max=max(AGE_VISITE0),
+    n=n()
+  )
+
+
+# AGE_DEBSYMPT
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(AGE_DEBSYMPT),
+    sd=sd(AGE_DEBSYMPT),
+    median=median(AGE_DEBSYMPT),
+    Q1 = quantile(AGE_DEBSYMPT, 0.25),
+    Q3 = quantile(AGE_DEBSYMPT, 0.75),
+    min=min(AGE_DEBSYMPT),
+    max=max(AGE_DEBSYMPT),
+    n=n()
+  )
+
+
+# DELAI_CHARGE
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% distinct() %>%
+  group_by(DIAGNIV) %>%
+  summarise(
+    mean=mean(DELAI_CHARGE),
+    sd=sd(DELAI_CHARGE),
+    median=median(DELAI_CHARGE),
+    Q1 = quantile(DELAI_CHARGE, 0.25),
+    Q3 = quantile(DELAI_CHARGE, 0.75),
+    min=min(DELAI_CHARGE),
+    max=max(DELAI_CHARGE),
+    n=n()
+  )
+
+
+AllMSA_Pop_Baseline_671 %>% inner_join(BaselineDem) %>% 
+  distinct() %>%
+  group_by(DIAGNIV, SEXE) %>% 
+  count()
+
+
+# --------------
